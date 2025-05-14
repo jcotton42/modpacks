e34ca25f43fd207346c00fb09fc305b40bf1e90a
Josh Cotton <jcotton42@outlook.com>
2025-05-13 21:18:12 -0700

    Finish zip script.

--- .ci/release.py
+++ .ci/release.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+
+import re
 import shutil
 import sys
 
@@ -17,5 +20,27 @@ mmc_pack_template = Template(ci.joinpath('mmc-pack.json').read_text())
 packs = Path(sys.argv[1])
 
 for pack in packs.iterdir():
+    if not pack.is_dir(): continue
     work = scratch.joinpath(pack.name)
-    
+    work.mkdir()
+    packToml = pack.joinpath('pack.toml').read_text()
+    mc = re.search(r'^minecraft *= *"(.+)"$', packToml, re.MULTILINE).group(1)
+    neo = re.search(r'^neoforge *= *"(.+)"$', packToml, re.MULTILINE).group(1)
+
+    mmc_pack = mmc_pack_template.substitute(mc=mc, neo=neo)
+    work.joinpath('mmc-pack.json').write_text(mmc_pack)
+    work.joinpath('instance.cfg').touch(mode=0o644)
+    patches = work.joinpath('patches')
+    patches.mkdir()
+    shutil.copy(
+        ci.joinpath('com.unascribed.unsup.json'),
+        patches
+    )
+    minecraft = work.joinpath('minecraft')
+    minecraft.mkdir()
+    shutil.copy(
+        pack.joinpath('unsup.ini'),
+        minecraft
+    )
+
+    shutil.make_archive(dist.joinpath(pack.name), 'zip', work)
