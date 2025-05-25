e224abc9c0cc5e3b365826e5ed39da5adace57ff
Josh Cotton <jcotton42@users.noreply.github.com>
2025-05-24 19:14:43 -0700

    CI and pages (#3)
    * CI start.
    
    * Finish zip script.
    
    * Fix gitignore.
    
    * Release script resets work dirs first.
    
    * Pages workflow.
    
    * Will the workflow show now?
    
    * Only deploy on main.
    
    * Sign packs.
    
    * Add public key to unsup.ini.
    
    * Make it an error to miss the signature.
    
    * Fail job on missing signature placeholder.
    
    * Check packs before doing anything else.
    
    * Add public key placeholder.
    
    * Testing error handling.
    
    * Fix stupid typos.
    
    * Fix sed command.
    
    * Error handling works.
    
    * Stupid bash syntax.
    
    * Also run workflow on PRs.
    
    * Remove need for public key placeholder.
    
    * Fix fail check.
    
    * Create launcher pack zips.
    
    * Test an index file.
    
    * Add license notice to index.
    
    * Build site index with links to launcher packs.
    
    * Use rsync instead of cp to skip symlinks.
    
    * Better index page layout.

---
+++ build/launcher-packs.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+import shutil
+import sys
+import tempfile
+import tomllib
+
+from pathlib import Path
+from string import Template
+
+build = Path(__file__).parent
+
+mmc_pack_template = Template(build.joinpath('mmc-pack.json').read_text())
+
+packs = Path(sys.argv[1])
+dist = Path(sys.argv[2])
+scratch = Path(tempfile.mkdtemp(dir=sys.argv[3]))
+
+dist.mkdir()
+
+for pack in packs.iterdir():
+    if not pack.is_dir(): continue
+    work = scratch.joinpath(pack.name)
+    work.mkdir()
+    with pack.joinpath('pack.toml').open('rb') as f:
+        packToml = tomllib.load(f)
+        mc = packToml['versions']['minecraft']
+        neo = packToml['versions']['neoforge']
+
+    mmc_pack = mmc_pack_template.substitute(mc=mc, neo=neo)
+    work.joinpath('mmc-pack.json').write_text(mmc_pack)
+    work.joinpath('instance.cfg').touch(mode=0o644)
+    patches = work.joinpath('patches')
+    patches.mkdir()
+    shutil.copy(
+        build.joinpath('com.unascribed.unsup.json'),
+        patches
+    )
+    minecraft = work.joinpath('minecraft')
+    minecraft.mkdir()
+    shutil.copy(
+        pack.joinpath('unsup.ini'),
+        minecraft
+    )
+
+    shutil.make_archive(dist.joinpath(pack.name), 'zip', work)
